1f2ee6b3a60c350af8157c9d1775dd1382cdc907
vti <viacheslav.t@gmail.com>
2009-07-28 18:00:39 +0200

    created date support, nice links

--- blog.pl
+++ blog.pl
@@ -3,6 +3,7 @@
 use Mojolicious::Lite;
 use Mojo::Date;
 use Pod::Simple::HTML;
+require Time::Local;
 
 my %config = (
     name        => 'vti',
@@ -25,39 +26,40 @@ get '/:index' => {index => 'index'} => 'index' => sub {
         my $data = _parse_article("$root/$file");
         next unless $data;
 
-        my ($name) = ($file =~ m/^(.*?)\.pod/);
-
-        push @articles,
-          { name  => $name,
-            mtime => Mojo::Date->new((stat("$root/$file"))[9]),
-            %$data
-          };
+        push @articles, $data;
     }
 
     @articles = sort { $b->{mtime}->epoch <=> $a->{mtime}->epoch } @articles;
 
     my $last_modified = $articles[0]->{mtime};
 
-    return 1 unless _is_modified($c, $last_modified);
+    #return 1 unless _is_modified($c, $last_modified);
 
     $c->stash(articles => \@articles, config => \%config);
 
     $c->res->headers->header('Last-Modified' => $last_modified);
 };
 
-get '/articles/:file' => 'article' => sub {
+get '/articles/:year/:month/:day/:alias' => 'article' => sub {
     my $c = shift;
 
     my $root = $c->app->home->rel_dir('articles');
-    my $path = "$root/" . $c->stash('file') . '.pod';
-    my $last_modified = Mojo::Date->new((stat($path))[9]);
+    my $path = join('/',
+        $root,
+        $c->stash('year')
+          . $c->stash('month')
+          . $c->stash('day') . '-'
+          . $c->stash('alias')
+          . '.pod');
 
     return $c->app->static->serve_404($c) unless -r $path;
+
+    my $last_modified = Mojo::Date->new((stat($path))[9]);
 
     my $data; $data = _parse_article($path)
         or return $c->app->static->serve_404($c);
 
-    return 1 unless _is_modified($c, $last_modified);
+    #return 1 unless _is_modified($c, $last_modified);
 
     $c->stash(article => $data, template => 'article', config => \%config);
 
@@ -85,6 +87,13 @@ sub _parse_article {
 
     return $_articles{$path} if $_articles{$path};
 
+    return unless $path =~ m/\/(\d\d\d\d)(\d\d)(\d\d)-(.*?)\.pod/;
+    my ($year, $month, $day, $name) = ($1, $2, $3, $4);
+
+    my $epoch = 0;
+    eval { $epoch = Time::Local::timegm(0, 0, 0, $day, $month, $year); };
+    return if $@ || $epoch < 0;
+
     my $parser = Pod::Simple::HTML->new;
 
     $parser->force_title('');
@@ -101,7 +110,16 @@ sub _parse_article {
 
     $title = $parser->get_title;
 
-    return $_articles{$path} = {title => $title, content => $content};
+    return $_articles{$path} = {
+        title   => $title,
+        content => $content,
+        mtime   => Mojo::Date->new((stat($path))[9]),
+        created => Mojo::Date->new($epoch),
+        year    => $year,
+        month   => $month,
+        day     => $day,
+        name    => $name
+    };
 }
 
 app->types->type(rss => 'application/rss+xml');
@@ -116,8 +134,8 @@ __DATA__
 <h1>Articles</h1>
 <ul>
 % foreach my $article (@$articles) {
-    <li><a href="/articles/<%== $article->{name} %>.html"><%= $article->{title} || $article->{name} %></a>
-    Last modified: <%= $article->{mtime} %></li>
+    <li><a href="/articles/<%== join('/', $article->{year}, $article->{month}, $article->{day}, $article->{name}) %>.html"><%= $article->{title} || $article->{name} %></a>
+    Created: <%= $article->{created} %></li>
 % }
 </ul>
 
